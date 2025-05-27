0772834e66ac75fa25cbe0db7f249be5cfb705d6
ChoisMath <complete860127@gmail.com>
2025-05-28 01:43:17 +0900

    downloadfixgggg

--- pages/📝상담일지.py
+++ pages/📝상담일지.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns   
 import gspread
 from google.oauth2.service_account import Credentials
 
